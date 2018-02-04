f8f2c7bd4d7bac93cadfe1fc426e7ee7034ac9f5
Anonymous Applicant <anonymous@example.com>
2018-02-04 15:54:36 -0500

    Add function to get city and state parameters from form inputs

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -2,13 +2,22 @@
   (:require [hiccup.page :refer [html5]]
             [my-exercise.home :as home]))
 
-(defn search-results [_]
-  "Function to render search results"
-  [:div {:class "search-results"}
-   [:h1 "Search Results"]])
+(defn getParams
+  "Function to get incoming form params"
+  [{city :city state :state}]
+  {:state state :place city})
 
-(defn page [request]
+(defn search-results
+  "Function to render search results"
+  [request]
+  (let [search-params (getParams (:params request))]
+    (println search-params)
+    [:div {:class "search-results"}]
+    [:h1 "Search Results"]))
+
+(defn page
   "Function to render search page"
+  [request]
   (html5
    (home/header request)
    (search-results request)))
